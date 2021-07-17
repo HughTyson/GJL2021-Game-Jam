038074d90c6f2d162cd825cfd2001cadd4eb6a38
Hugh T <1700792@uad.ac.uk>
2021-07-17 10:37:12 +0100

    Chagned To RigidBody

--- GJLProject/Assets/Scripts/Player Scripts/CharacterMovement.cs
+++ GJLProject/Assets/Scripts/Player Scripts/CharacterMovement.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
 
-    CharacterController controller;
-    Vector3 movement = Vector3.zero;
+    Rigidbody controller;
 
     [SerializeField] float movement_speed = 6f;
-    [SerializeField] float jump_force = 10f;
-    [SerializeField] float gravity = 10;
+    [SerializeField] float jump_height = 10f;
+   
+
+    bool is_grounded;
 
     Vector3 movement_direction = Vector3.zero;
 
@@ -21,7 +22,8 @@ public class CharacterMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        controller = GetComponent<Rigidbody>();
+        
         look_right = transform.rotation;
         look_left = transform.rotation * Quaternion.Euler(0, 180, 0); ;
     }
@@ -31,25 +33,33 @@ public class CharacterMovement : MonoBehaviour
     {
 
         HandleInput();
+
+        //check if the character is grounded
+
         
-        Jump();
+        
+        
         Move();
+        Jump();
         RotateCharacter();
     }
 
     private void Move()
     {
-        if(!controller.isGrounded)
-            movement_direction.y -= gravity * Time.deltaTime;
-
-        controller.Move(movement_direction * Time.deltaTime);
+controller.MovePosition(controller.position + movement_direction * movement_speed * Time.fixedDeltaTime);
+       
     }
 
     private void Jump()
     {
-        if (controller.isGrounded)
-            if (Input.GetKey(KeyCode.UpArrow))
-                movement_direction.y = jump_force;
+        float DisstanceToTheGround = GetComponent<Collider>().bounds.extents.y;
+        bool is_grounded = Physics.Raycast(transform.position, Vector3.down, DisstanceToTheGround + 0.1f);
+
+        if (movement_direction.z == 1 && is_grounded)
+        {
+            controller.AddForce(Vector3.up * Mathf.Sqrt(jump_height * -2f * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+        
 
     }
 
@@ -64,11 +74,14 @@ public class CharacterMovement : MonoBehaviour
         {
             transform.rotation = look_left;
         }
+
+        
     }
 
     private void HandleInput()
     {
-        movement_direction.x = Input.GetAxisRaw("Horizontal") * movement_speed;
+        movement_direction.x = Input.GetAxisRaw("Horizontal");
+        movement_direction.z = Input.GetAxisRaw("Vertical");
     }
 
 }
